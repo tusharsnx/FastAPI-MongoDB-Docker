63c60ca0e2d064c8d37ba4bb2b0b3b004850bfe7
Tushar Singh <tusharvickey1999@gmail.com>
2021-07-10 21:34:36 +0530

    added port access in site.py

--- app/routers/site.py
+++ app/routers/site.py
@@ -14,7 +14,8 @@ DOMAIN = os.environ["DOMAIN"]
 if DOMAIN=="http://localhost:8000":
     INNER_DOMAIN = DOMAIN
 else:
-    INNER_DOMAIN = "http://0.0.0.0:8000"
+    PORT = os.environ["$PORT"]
+    INNER_DOMAIN = f"http://0.0.0.0:{PORT}"
 
 router = APIRouter(default_response_class=HTMLResponse, include_in_schema=False)
 template = Jinja2Templates(directory="templates")
