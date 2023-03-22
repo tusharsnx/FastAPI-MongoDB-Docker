3aeae5e041f5b9398cb197ec11756f1a9bceb75e
Tushar Singh <tusharvickey1999@gmail.com>
2023-03-22 13:21:38 +0530

    add debug lines for test deployment

--- app/routers/authorization.py
+++ app/routers/authorization.py
@@ -6,6 +6,7 @@ from uuid import uuid4
 import aiohttp
 import asyncio
 import os
+import pprint
 
 # loading config for authorization
 CLIENT_ID = os.environ["CLIENT_ID"]
@@ -139,4 +140,5 @@ async def create_new_user(name, username):
             if resp.status!=200:
                 # user does not exists
                 data = {"username": username, "name": name}
-                await session.post(f"{DOMAIN}/api/users", json=data)
+                resp = await session.post(f"{DOMAIN}/api/users", json=data)
+                pprint.pprint(resp.history)
